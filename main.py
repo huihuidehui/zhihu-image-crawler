b4b4afb51014da0c60842de10909070317e60f4e
kanhui <2271904191@qq.com>
2020-03-31 06:33:09 +0800

    添加睡眠

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import grequests
 import os
 import click
 import progressbar
-
+import time
 # sys.setrecursionlimit(1000000)  # 例如这里设置为一百万
 
 BASE_URL = "https://www.zhihu.com/api/v4/questions/{}/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cbadge%5B*%5D.topics&limit={}"
@@ -85,6 +85,8 @@ class ZhSpider(object):
                 bar = progressbar.ProgressBar(max_value=total_img_url)
                 # bar.update(i)
                 while len(img_urls) > 0:
+                    # 添加延时，保证知乎服务的正常运行
+                    time.sleep(0.5)
                     if len(img_urls) > 5:
                         tmp_img_urls = img_urls[0:5]
                         img_urls = img_urls[5:]
